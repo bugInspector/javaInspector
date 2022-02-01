f07f0ee14d7c2d8fba46f5ca2c77549f70f92042
Edward Baranetsky <ebaranetchi@alliedtesting.com>
2022-02-01 12:20:36 +0200

    initial commit

---
+++ car/src/main/java/org/bugInspector/javaInspector/Car.java
@@ -0,0 +1,37 @@
+package org.bugInspector.javaInspector;
+
+public class Car {
+    private final Window frontWindow;
+    private final String color;
+
+    public Car(Window frontWindow, String color) {
+        this.frontWindow = frontWindow;
+        this.color = color;
+    }
+
+    public Window getFrontWindow() {
+        return frontWindow;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Car car = (Car) o;
+
+        if (frontWindow != null ? !frontWindow.equals(car.frontWindow) : car.frontWindow != null) return false;
+        return color != null ? color.equals(car.color) : car.color == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = frontWindow != null ? frontWindow.hashCode() : 0;
+        result = 31 * result + (color != null ? color.hashCode() : 0);
+        return result;
+    }
+}
---
+++ core/src/main/java/org/bugInspector/javaInspector/Window.java
@@ -0,0 +1,28 @@
+package org.bugInspector.javaInspector;
+
+public class Window {
+    private final int size;
+
+    public Window(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Window window = (Window) o;
+
+        return size == window.size;
+    }
+
+    @Override
+    public int hashCode() {
+        return size;
+    }
+}
